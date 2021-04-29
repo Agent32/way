8785d3cbada8c53068efabf2f5f6e1d1e4763c3d
Artur <1008480224@ukr.net>
2021-04-29 23:00:58 +0300

    2904

--- src/easyWb/bodComp/users/usersContainer.js
+++ src/easyWb/bodComp/users/usersContainer.js
@@ -1,28 +1,25 @@
-import React from "react";
-import { connect } from "react-redux";
 
-import { changeUsrFollowAction, getMoreUsrAction } from "../../../redux/userReducer";
-import UsersPage from "./usersSearch";
+import { connect } from 'react-redux'
 
+import { changeUsrFollowAction, getMoreUsrAction } from '../../../redux/userReducer'
+import UsersPage from './usersSearch'
 
 // ========================================
 const mapStateToProps = (state) => {
-  return { usersList: state.usersPart.usersList
-   };
-};
-//-----------------------------------------
+  return { usersList: state.usersPart.usersList }
+}
+// -----------------------------------------
 const mapDispatchToProps = (dispatch) => {
   return {
     updateUserChange: (id) => dispatch(getMoreUsrAction(id)),
-    userFollowChange: (id) => dispatch(changeUsrFollowAction(id)),
-    
-  };
-};
+    userFollowChange: (id) => dispatch(changeUsrFollowAction(id))
+
+  }
+}
 // ========================================
 const UserContainer = connect(
   mapStateToProps,
   mapDispatchToProps
-)(UsersPage);
+)(UsersPage)
 
-
-export default UserContainer;
+export default UserContainer
