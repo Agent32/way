9accdeca47315ebed3fc2754eccae1eae4ca575f
Artur <1008480224@ukr.net>
2021-04-29 23:01:20 +0300

    2904

--- src/easyWb/bodComp/users/usersSearch.jsx
+++ src/easyWb/bodComp/users/usersSearch.jsx
@@ -1,61 +1,54 @@
 import users from "./users.module.css";
 import * as axios from "axios";
+import React from "react";
 //-----------------------------------------
-function UsersPage(props) {
-
-  function getTask() {
- 
-  return  axios
-      .get('https://60885809a6f4a300174263e9.mockapi.io/Test')
+class UsersPage extends React.Component {
+  constructor(props) {
+    super(props);
+    axios
+      .get("https://60885809a6f4a300174263e9.mockapi.io/Test")
       .then((res) => {
-        return props.updateUserChange(res.data)  
-       
-      })
+        this.props.updateUserChange(res.data);
+      });
   }
-  
-/*   function ebani (res) {
-   props.updateUsers(res.data)   
-  } */
- 
-  
 
- debugger
-  
-  const formUsers = props.usersList.map((currUsers, count) => {
-    return (
-      <div className={users.post} key={currUsers.id}>
-        <img src={currUsers.avatarImg} />
-
-        <div className={users.name}>
-          {` ${currUsers.name} ${currUsers.secondName}`}{" "}
-        </div>
-
-        <div className={users.adress}>
-          {" "}
-          {` ${currUsers.adressCountry} ${currUsers.adressCity}`}
-          <button onClick={() => props.userFollowChange(currUsers.id)}>
-            {" "}
-            {currUsers.isFollow ? "UnSubscribe" : "Subscribe"}
-          </button>
-        </div>
-
-        <div className={users.quote}> {` ${currUsers.userQuote} `} </div>
-      </div>
-    );
-  });
 
   //-----------------------------------------//-----------------------------------------
-  return (
-    <div className={users.main}>
-      {formUsers}
-      <div>
-        {" "}
-        <button onClick={() => getTask() } />{" "}
+  render() {
+    const formUsers = this.props.usersList.map((currUsers, count) => {
+      return (
+        <div className={users.post} key={currUsers.id}>
+          <img src={currUsers.avatarImg} />
+  
+          <div className={users.name}>
+            {` ${currUsers.name} ${currUsers.secondName}`}{" "}
+          </div>
+  
+          <div className={users.adress}>
+            {" "}
+            {` ${currUsers.adressCountry} ${currUsers.adressCity}`}
+            <button onClick={() => this.props.userFollowChange(currUsers.id)}>
+              {" "}
+              {currUsers.isFollow ? "UnSubscribe" : "Subscribe"}
+            </button>
+          </div>
+  
+          <div className={users.quote}> {` ${currUsers.userQuote} `} </div>
+        </div>
+      );
+    });
+
+    
+    return (
+      <div className={users.main}>
+        {formUsers}
+        <div>
+          {" "}
+          <button onClick={() => alert(1)} />{" "}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-
 
 export default UsersPage;
